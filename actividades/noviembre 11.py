377a2f3e514b6b2ec08a5372d80e30c47ac6b2b8
james <jamesalejandromelendez@hotmail.com>
2022-11-13 16:32:36 -0500

    .

---
+++ actividades/noviembre 11.py
@@ -0,0 +1,77 @@
+"""import random
+def ascendente (x,y):
+    if x<y:
+        return"asendente"
+    elif x>y:
+        return "desendente"
+    else:
+        return "iguales"
+print(ascendente(1,5))
+print(ascendente(10,5))
+print(ascendente(10,10))
+for i in range(10):
+    x=round(random.randrange(100))
+    y=round(random.randrange(100))
+    print(x," ",y,ascendente(x,y))"""
+    
+"""def sumadivisores(num):
+    suma=0
+    for i in range(1,num):
+        if num%i==0:
+            suma+=i
+    return suma
+print(sumadivisores(220))
+def perfectos(num):
+    sum=sumadivisores(num)
+    if sum == num:
+        return "es perfecto"
+    else:
+        return "no es perfecto "
+print (perfectos(20))"""
+
+
+"""def amigos(y):
+    y=int(input("xxx"))
+    if sumadivisores==y:
+        return "son amigos"
+    else:
+        return "no son amigos"
+print(amigos)"""
+
+
+"""def sumadivisores(num):
+    for i in range (1,100):
+
+def amigos(x,y):
+    sumx=sumadivisores(x)
+sumy=sumadivisores(y)
+if sumx==y and sumy==x:
+    return "son amigos"
+else:
+    return "no son amigos"""
+    
+
+"""def primos(num):
+    sum=sumadivisores(num)
+    if sum==1:
+        return "es primo"
+    else:
+        return "no es primo "
+print (primos(12))"""
+    
+"""import random
+def llenarlista(list):
+    tam=round(random.randint(5,20))
+    for i in range (tam):
+        list.append(round(random.randrange(100)))
+    return list
+
+def sumarlista(list):
+    sum=0
+    for i in list:
+        sum+=i
+    return sum
+lista=[]
+lista=llenarlista(lista)
+print(lista)
+print("la suma es= ",sumarlista(lista))"""
--- actividades/practicabase de datos.py
+++ actividades/practicabase de datos.py
@@ -1,7 +1,11 @@
-my_list = [3, 7] 
-val = int(input("inserte su numero"))
-print (val in my_list)
-if val == 0:
-    print ("su numero no esta en el sistema")
-else val < 0:
-
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+val = int(input("su numero es  "))
+if val in my_list:
+    print ("su numero si esta en nuesta lista")
+else:
+    print ("su numero no esta en el sistema, desea agregarlo?")
+    res = input("responde con un si o un no ")
+    if res == "si":
+            my_list.append(val)
+            print (my_list)
+            print ("su numero a sido agregado exitosamente")
